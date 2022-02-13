391616f086fe3e1b9689308068fb40cfc51f113e
AntonS <you@example.com>
2022-02-13 03:35:53 +0200

    initial commit

--- script.js
+++
@@ -1,5 +0,0 @@
-let loginHeader = document.querySelector("#loginHeader");
-let loginForm = document.querySelector(".loginForm");
-loginHeader.addEventListener('click', () => {
-    document.querySelector(".loginForm").classList.add("active")
-});
